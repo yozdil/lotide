6f3af310dd06b3a80b5b186faf9ab1298af1079e
Yamac Ozdil <yamac.ozdil@outlook.com>
2021-02-01 20:03:08 +0000

    Final Version

--- assertEqual.js
+++ assertEqual.js
@@ -6,3 +6,7 @@ const assertEqual = function(actual, expected) {
     console.log(`❗️❗️❗️Assertion Failed: [${actual}] !== [${expected}]`);
   }
 };
+
+// TEST CODE
+console.assert(sum(1, 2) === 3);
+console.assert(sum(1, 20) === 3); // bad / incorrect assertion, and we see it fail!
