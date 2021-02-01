33a9d70b2a0b3b913a0d622f298860c56e0687c6
Yamac Ozdil <yamac.ozdil@outlook.com>
2021-02-01 19:30:32 +0000

    Comment change

--- assertEqual.js
+++ assertEqual.js
@@ -1,4 +1,4 @@
-// FUNCTION IMPLEMENTATION
+// ASSERT EQUAL FUNCTION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅️✅️✅️Assertion Passed: [${actual}] === [${expected}]`);
