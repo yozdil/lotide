dd882b6099f265122f06266013a724bf400b536f
Yamac Ozdil <yamac.ozdil@outlook.com>
2021-02-02 21:20:13 +0000

    Equal arrays function

---
+++ eqArrays.js
@@ -0,0 +1,16 @@
+// EQUAL ARRAYS FUNCTION
+
+const eqArrays = (arr1, arr2) => {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr2[i] !== arr1[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+eqArrays([1, 2, 3], [1, 2, 3]); // => true
+eqArrays([1, 2, 3], [3, 2, 1]); // => false
+
+eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
