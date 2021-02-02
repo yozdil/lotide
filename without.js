54d6081289bab787a3847d9b6fa8b5719d7143f0
Yamac Ozdil <yamac.ozdil@outlook.com>
2021-02-02 22:24:45 +0000

    Implement without

---
+++ without.js
@@ -0,0 +1,20 @@
+const without = (source, itemsToRemove) => {
+  // We use the spread operator to copy the first array that the values will be
+  // removed from.
+  let withoutArray = [...source];
+  // In a nested for loop we iterate through each array to take out the values
+  // existing in the second array from the first array
+  for (let i = source.length - 1; i > -1; i--) {
+    for (let j = 0; j < itemsToRemove.length; j++) {
+      if (source[i] === itemsToRemove[j]) {
+        withoutArray.splice(i, 1);
+      }
+    }
+  }
+  return withoutArray;
+};
+
+// TEST CASES
+
+console.log(without([1, 2, 3], [1])); // => [2, 3]
+console.log(without(["1", "2", "3"], [1, 2, "3"])); // => ["1", "2"]
