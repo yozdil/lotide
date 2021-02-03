935f844c341f316cb8d641afd72914827b39452a
Yamac Ozdil <yamac.ozdil@outlook.com>
2021-02-03 23:08:31 +0000

    Assert Arrays Equal

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,21 +1,19 @@
-// EQUAL ARRAYS FUNCTION
-
-const eqArrays = (arr1, arr2) => {
-  if (arr1.length === arr2.length) {
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr2[i] !== arr1[i]) {
-        return false;
-      }
-    }
-    return true;
-  } else {
-    return false;
-  }
-};
-
 // EQUAL ARRAYS ASSERTION FUNCTION
-
 const assertArraysEqual = (arr1, arr2) => {
+  // EQUAL ARRAYS FUNCTION
+  const eqArrays = (arr1, arr2) => {
+    if (arr1.length === arr2.length) {
+      for (let i = 0; i < arr1.length; i++) {
+        if (arr2[i] !== arr1[i]) {
+          return false;
+        }
+      }
+      return true;
+    } else {
+      return false;
+    }
+  };
+
   if (eqArrays(arr1, arr2)) {
     console.log(`✅️✅️✅️Assertion Passed: [${arr1}] === [${arr2}]`);
   } else {
@@ -24,6 +22,5 @@ const assertArraysEqual = (arr1, arr2) => {
 };
 
 // TEST CODE
-
 assertArraysEqual([1, 2, 3], [1, 2, 3]);
 assertArraysEqual([1, "a", 3], [1, 5, 3]);
