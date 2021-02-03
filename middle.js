771ac4e89f8b5a382043272da12e8ab06583bb4b
Yamac Ozdil <yamac.ozdil@outlook.com>
2021-02-03 14:38:33 +0000

    Test codes

--- middle.js
+++ middle.js
@@ -20,3 +20,5 @@ console.log(middle([1, 2, 3, 4, 5])); // => [3]
 
 console.log(middle([1, 2, 3, 4])); // => [2, 3]
 console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
+
+console.log(typeof middle);
