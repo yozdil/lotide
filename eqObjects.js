5ed46f1f1f203668920baaa56b36fe7f79ce6525
Yamac Ozdil <yamac.ozdil@outlook.com>
2021-02-06 00:22:07 +0000

    eqObjects Recursive

--- eqObjects.js
+++ eqObjects.js
@@ -14,36 +14,55 @@ const eqObjects = (obj1, obj2) => {
     }
   };
 
-  const keys = Object.keys(obj1);
   // If the objects do not have the same amount of keys return false, they
   // cannot be equal.
-  if (keys.length !== Object.keys(obj2).length) {
+  if (Object.keys(obj1).length !== Object.keys(obj2).length) {
     return false;
   } else {
-    for (const key of keys) {
+    for (const key of Object.keys(obj1)) {
       // If the values for the key are not the same for both objects
       if (obj1[key] !== obj2[key]) {
         // Is it an Array?
         if (Array.isArray(obj1[key])) {
           // Return the variable that takes the result of eqArrays function.
-          const x = eqArrays(obj1[key], obj2[key]);
-          if (!x) {
+          if (!eqArrays(obj1[key], obj2[key])) {
+            return false;
+          }
+        } else if (
+          // So it's not an array but are both values still an object?
+          typeof obj1[key] === "object" &&
+          typeof obj2[key] === "object"
+        ) {
+          if (!eqObjects(obj1[key], obj2[key])) {
             return false;
           }
         } else {
-          // It is not an array then the primitive values are not equal to each
+          // It is not an array nor an object then the primitive values are not equal to each
           // other.
           return false;
         }
       }
     }
+    return true;
   }
-  return true;
 };
 
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
-console.log(eqObjects(cd, dc)); // => true
+// TEST CASES
 
-const cd2 = { c: "1", d: ["2", 3, 4] };
-console.log(eqObjects(cd, cd2)); // => false
+console.log(
+  eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }) // => true
+);
+console.log(
+  eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }) // => false
+);
+console.log(
+  eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }) // => false
+);
+
+
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+console.log(eqObjects(ab, ba)); // => true
+
+const abc = { a: "1", b: "2", c: "3" };
+console.log(eqObjects(ab, abc)); // => false
