1f2c614b7513f25602affca40066eee4975e8d30
Yamac Ozdil <yamac.ozdil@outlook.com>
2021-02-08 22:57:58 +0000

    V1.0.1

--- flatten.js
+++ flatten.js
@@ -1,18 +1,13 @@
 // FLATTEN ARRAY  FUNCTION
 const flatten = (arr) => {
-  // Only handles one level of nested arrays.
-  let flatArray = [];
-  for (const el of arr) {
-    if (Array.isArray(el)) {
-      // If the element is indeed an array loop through and push into the new array.
-      for (let i = 0; i < el.length; i++) {
-        flatArray.push(el[i]);
-      }
-    } else {
-      flatArray.push(el);
+  let flat = arr.reduce((total, cur) => {
+    if (Array.isArray(cur)) {
+      return total.concat(flatten(cur));
     }
-  }
-  return flatArray;
+    return total.concat(cur);
+  }, []);
+
+  return flat;
 };
 
 module.exports = flatten;
--- without.js
+++ without.js
@@ -1,16 +1,16 @@
 const without = (source, itemsToRemove) => {
-  // We use the spread operator to copy the first array that the values will be
-  // removed from.
   let withoutArray = [...source];
-  // In a nested for loop we iterate through each array to take out the values
-  // existing in the second array from the first array
-  for (let i = source.length - 1; i > -1; i--) {
-    for (let j = 0; j < itemsToRemove.length; j++) {
-      if (source[i] === itemsToRemove[j]) {
+  withoutArray.filter((val, i) => {
+    for (let item of itemsToRemove) {
+      if (val === item) {
         withoutArray.splice(i, 1);
       }
     }
-  }
+  });
   return withoutArray;
 };
+
+without([1, 2, 3], [1]); //?
+without(["1", "2", "3"], [1, 2, "3"]); //?
+
 module.exports = without;
